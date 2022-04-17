7f1bf52bdb3980d6a4571f83e41a5e184220695b
yos10 <61726023+yos10@users.noreply.github.com>
2022-04-17 12:09:04 +0900

    WIP
    task-list.js 完成

--- 01-mid/task-list.js
+++ 01-mid/task-list.js
@@ -9,31 +9,31 @@ function addTask(task) {
 }
 
 /**
- * 
- * @param {string} id 
+ *
+ * @param {string} id
  */
 function deleteTask(id) {
-  const deletedTasks = getTasks().filter(task => task.id !== id);
+  const filteredTasks = getTasks().filter((task) => task.id !== id);
 
-  tasks = deletedTasks;
+  tasks = filteredTasks;
 }
 
 function createHtmlElement(html) {
   const template = document.createElement('template');
-  template.innerHTML = html;
+  template.innerHTML = html.trim();
 
   return template.content.firstElementChild;
 }
 
 function removeHtmlElement(querySelector) {
-  document.querySelectorAll(querySelector).forEach(e => e.remove());
+  document.querySelectorAll(querySelector).forEach((e) => e.remove());
 }
 
-function displayTaskList () {
+function displayTaskList() {
   removeHtmlElement('.taskrow');
 
   const tasks = getTasks();
-  tasks.forEach(t => {
+  tasks.forEach((t) => {
     const html = `
       <tr class="taskrow">
         <td>${t.taskmonth}</td>
@@ -45,7 +45,7 @@ function displayTaskList () {
     `;
     const task = createHtmlElement(html);
     document.querySelector('#tasklist').appendChild(task);
-  }); 
+  });
 }
 
 function addSample() {
@@ -54,26 +54,30 @@ function addSample() {
     taskmonth: '2021-07',
     taskstatus: '済',
     tasktitle: 'A社経営統合プロジェクト',
-    taskdetail: '経営統合に伴う業務プロセス統合プロジェクト。<br>プロジェクトリーダー（メンバー４人）として担当。<br>ＱＤＣ目標いずれも達成。ＣＳ評価で５をいただいた。'
-  }
+    taskdetail:
+      '経営統合に伴う業務プロセス統合プロジェクト。<br>プロジェクトリーダー（メンバー４人）として担当。<br>ＱＤＣ目標いずれも達成。ＣＳ評価で５をいただいた。',
+  };
   addTask(task);
   displayTaskList();
 }
 
+// 登録ボタンを押した時
 const form = document.querySelector('#form');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   const task = {};
-  
-  const formElements = Array.from(form.elements).filter(element => element.id !== 'submit');
+
+  const formElements = Array.from(form.elements).filter(
+    (element) => element.id !== 'submit'
+  );
   for (const element of formElements) {
     const elementId = element.id;
     const value = element.value;
     task[elementId] = value;
 
     // 入力エリアを初期化
-    element.value = ''
+    element.value = '';
   }
 
   task['id'] = Date.now().toString();
@@ -82,6 +86,7 @@ form.addEventListener('submit', (e) => {
   displayTaskList();
 });
 
+// 削除ボタンを押した時
 const tasklist = document.querySelector('#tasklist');
 // Event Delegation
 tasklist.addEventListener('click', (e) => {
